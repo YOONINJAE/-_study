0debccdc6d226a525f4948ec950bf5eb10a822ae
YOONINJAE <71324062+YOONINJAE@users.noreply.github.com>
2021-01-07 12:36:37 +0900

    스위치 테스트

---
+++ test.ino
@@ -0,0 +1,16 @@
+const int btn1 =2;
+const int led1 = 13;
+bool state = 0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led1,OUTPUT);
+  pinMode(btn1,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  state = digitalRead(btn1);
+  if(state == HIGH) digitalWrite(led1,HIGH);
+  else digitalWrite(led1,LOW);
+  //gd
+}
